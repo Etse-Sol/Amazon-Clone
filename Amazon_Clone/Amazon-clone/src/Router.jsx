988d195635b4e551e174b8d4d9988a5f247341a4
Etse-Sol <solomonetsegenet7@gmail.com>
2024-11-29 23:45:27 +0300

    payment

--- Amazon_Clone/Amazon-clone/src/Router.jsx
+++ Amazon_Clone/Amazon-clone/src/Router.jsx
@@ -8,16 +8,21 @@ import Payment from './page/Payment/Payment'
 import Results from './page/Results/Results'
 import ProductDeail from './page/ProductDetails/ProductDeail'
 import Auth from './page/Auth/Auth'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+const stripePromise = loadStripe(
+  "pk_test_51QQLKyEpKgVXSvkkw3GJTtVzEVwgEZSgjMA0D1D0l6JG6j1TNfpFfkAI0yW4PZCU4ARTpCKMZyX4L96NpClgIzYc000C2ArKC2"
+);
 function Routering() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route path="/payment" element={<Elements stripe={stripePromise}><Payment /></Elements>} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
-        <Route path="/products/:productId" element={<ProductDeail/>} />
+        <Route path="/products/:productId" element={<ProductDeail />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
--- Amazon_Clone/Amazon-clone/src/page/Payment/Payment.jsx
+++ Amazon_Clone/Amazon-clone/src/page/Payment/Payment.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import Layout from '../../Components/Layout/Layout'
+import Classes from "./payment.module.css"
+import { DataContext } from '../../Components/DataProvider/DataProvider'
+import ProductCard from '../../Components/Product/ProductCard'
+import { useElements, useStripe , CardElement} from '@stripe/react-stripe-js'
+import CurrencyFormat from '../../Components/Product/CurrencyFormat/CurrencyFormat'
 
 function Payment() {
+  const [{basket,user},dispatch]=useContext(DataContext)
+  const totalItem=basket?.reduce((amount,item)=>{
+    return item.amount+amount
+  },0)
+   const total = basket.reduce((amount, item) => {
+     return item.price * item.amount + amount;
+   }, 0);
+  const [carderror,setCarderror]=useState(null)
+   const stripe= useStripe()
+   const elements= useElements()
+  const handlechange = (e) => {
+    setCarderror(e?.error?.message || "");
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Layout>
+      <br />
+      <br />
+      <div className={Classes.payment_header}>checkout({totalItem})items</div>
+      <section className={Classes.payment}>
+        <div className={Classes.flex}>
+          <h3>Delivary Address</h3>
+          <div>
+            <div>{user?.email}</div>
+            <div>123 React Lane</div>
+            <div>Chicago,IL</div>
+          </div>
+        </div>
+        <hr />
+        <div className={Classes.flex}>
+          <h3>Review items and Delivary</h3>
+          <div>
+            {basket?.map((item) => (
+              <ProductCard product={item} flex={true} />
+            ))}
+          </div>
+        </div>
+        <hr />
+        <div className={Classes.flex}>
+          <h3>Payment method</h3>
+          <div className={Classes.payment_card_container}>
+            <div className={Classes.payment_details}>
+              <form action="">
+                {carderror && (
+                  <small style={{ color: "red" }}> {carderror}</small>
+                )}
+                <CardElement onChange={handlechange} />
+              </form>
+              <div className={Classes.payment_price}>
+                <div>
+                  <span style={{display:"flex", gap:"10px"}}>
+                    <p>
+                      Total Order | </p><CurrencyFormat amount={total} />
+                    
+                  </span>
+                </div>
+                <button>Pay Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
 export default Payment
